b3c5c4d9eaf5195abd6702ba3881bb57b90c48ad
Jibin Thomas <jibin.thomas@thesouledstore.com>
2020-04-19 21:42:21 +0530

    Fix: Incorrect dev api endpoint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   env: {
-    BASE_URL: isProd ? 'https://blog-management-systems.herokuapp.com' : 'http://localhost:3000',
+    BASE_URL: isProd ? 'https://blog-management-systems.herokuapp.com' : 'http://localhost:5000',
   },
 };
