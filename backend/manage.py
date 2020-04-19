7febf000d801f364898f3db7650aa8c5cdc99f30
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-04-19 14:14:48 +0530

    Add production deployment configurations

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app.models import setup_db
 from app.auth.routes import auth
 from app.user.routes import user
+
 
 '''
 setting up flask app with cors middleware and db connection
@@ -13,7 +16,11 @@ setup_db(app)
 CORS(app)
 app.register_blueprint(auth)
 app.register_blueprint(user)
+# for db migration when running manage.py
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
+    manager.run()
     app.run(host='0.0.0.0', port='5000')
